27e87be9e62d3494db530d412f46f4e1ced5ed77
alexander <alexander.2003@live.com>
2019-09-12 15:59:26 -0400

    first commit

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestProgram.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TestProgram.java
@@ -7,6 +7,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackable;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaTrackables;
 
+// start teleop
 @TeleOp
 public class TestProgram extends LinearOpMode {
     VuforiaLocalizer vuforia;
