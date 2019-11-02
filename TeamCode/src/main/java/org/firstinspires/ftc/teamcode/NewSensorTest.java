80af0974218af421e23bc89182f54d030d15c8e9
alexander gherardi <alexander.2003@live.com>
2019-11-02 10:18:04 -0400

    Add test for adding new sensors

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewSensorTest.java
@@ -0,0 +1,9 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.configuration.annotations.DeviceProperties;
+import com.qualcomm.robotcore.hardware.configuration.annotations.I2cDeviceType;
+
+@I2cDeviceType
+@DeviceProperties(name = "TestSensor", description = "A Test Sensor", xmlTag = "NewSensor")
+public class NewSensorTest {
+}
