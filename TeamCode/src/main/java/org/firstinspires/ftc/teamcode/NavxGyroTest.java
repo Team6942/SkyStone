c346746847e1a2b37c86ee712774e2ef905b41df
Alexander Gherardi <alexander.2003@live.com>
2019-11-04 15:30:24 -0500

    Test Adding Accel to navx sensor Add drive to NavxGyroTest

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NavxGyroTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NavxGyroTest.java
@@ -2,29 +2,62 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.kauailabs.NavxMicroNavigationSensor;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.IntegratingGyroscope;
+import com.qualcomm.robotcore.util.Range;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesOrder;
 import org.firstinspires.ftc.robotcore.external.navigation.AxesReference;
 import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
 
+@TeleOp
 public class NavxGyroTest extends LinearOpMode {
+    DcMotor backLeft;
+    DcMotor backRight;
+    private double leftPower;
+    private double rightPower;
+    private double drive;
+    private double turn;
     private IntegratingGyroscope gyro;
     private NavxMicroNavigationSensor navxMicro;
+
     @Override
     public void runOpMode() {
         telemetry.setAutoClear(true);
         initNavx();
+        backLeft = hardwareMap.get(DcMotor.class, "backLeft");
+        backRight = hardwareMap.get(DcMotor.class, "backRight");
+        backLeft.setDirection(DcMotorSimple.Direction.FORWARD);
+        backRight.setDirection(DcMotorSimple.Direction.FORWARD);
+        backLeft.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backRight.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        backLeft.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        backRight.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
         waitForStart();
         while (opModeIsActive()) {
+            drive = gamepad1.right_stick_y;
+            turn = gamepad1.left_stick_x;
+
+            leftPower = Range.clip(drive + turn,-1,1);
+            rightPower = Range.clip(drive - turn,-1,1);
+
+            backLeft.setPower(leftPower);
+            backRight.setPower(rightPower);
+
+            telemetry.addData("Path2",  "%7d :%7d",
+                    backLeft.getCurrentPosition(),
+                    backRight.getCurrentPosition());
+
             telemetry.addData("gyro",getYaw());
             telemetry.update();
         }
     }
     private void initNavx() {
         navxMicro = hardwareMap.get(NavxMicroNavigationSensor.class, "navx");
-        gyro = (IntegratingGyroscope)navxMicro;
+        gyro = navxMicro;
         telemetry.log().add("do not move robot navx is calibrating");
         while (navxMicro.isCalibrating());
         telemetry.log().clear();
